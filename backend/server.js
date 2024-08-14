80b9a7f72546765267183ddf4251aec6a51f4f87
DESKTOP-I8B510D\sela <betsela555@gmail.com>
2024-08-14 19:48:08 +0700

    add or to switch to 3001 port

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ client
 const app = require('./app');
 
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3001;
 
 app.listen(port, () => {
   console.log(`LISTEN ON PORT: ${port}!`);
